7c2395c7075d98a115e86daef4391ca17a57ae03
Ivan Ivanchenko <i.ivanchenko1997@gmail.com>
2021-01-17 14:38:04 +0300

    Initial Commit

---
+++ Swift UI Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI Example
+//
+//  Created by Ivan Ivanchenko on 17.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift UI Example/Swift_UI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_ExampleApp.swift
+//  Swift UI Example
+//
+//  Created by Ivan Ivanchenko on 17.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
